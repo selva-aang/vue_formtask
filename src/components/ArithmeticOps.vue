c31439b83352474193942ef48494cfa36e0123ab
selva-aang <selvakumar@ngpwebsmart.com>
2022-08-19 11:39:45 +0530

    task completed

---
+++ src/components/ArithmeticOps.vue
@@ -0,0 +1,95 @@
+ <template>
+  <div class="hello">
+    <h1>{{ arithmetic }}</h1>
+    
+   <input type="number" v-model="num1">
+    {{num1}}<br><br>
+    
+   <input type="number" v-model="num2">
+     {{num2}}<br><br>
+    
+      <button @click="add">Add</button>
+      <button @click="sub">Subtract</button>
+      <button @click="mul">Multiply</button>
+      <button @click="div">Divide</button>
+      <p v-if="result>0" class="positive">{{ resultString }} {{ result }}</p>
+      <p v-else-if="result<0" class="negative">{{ resultString }} {{ result }}</p>
+      <p v-else>{{ resultString }}{{result}}</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+   arithmetic : Number
+  },
+  data(){
+    return{
+      num1: '',
+      num2: '',
+      result: '',
+      resultString: '',
+      sum: '',
+      diff: '',
+      prod: '',
+      quot: ''
+    }
+  },
+    methods:{
+    calculate(){
+      this.sum = (Number(this.num1)+Number(this.num2))
+      this.diff = (this.num1-this.num2)
+      this.prod = (this.num1*this.num2)
+      this.quot = (this.num1/this.num2)
+    },
+    add(){
+      this.result = (Number(this.num1)+Number(this.num2))
+      console.log(this.result);
+      this.resultString = "Sum :"
+    },
+    sub(){
+      this.result = (this.num1-this.num2)
+      this.resultString = "Difference :"
+    },
+    mul(){
+      this.result = (this.num1*this.num2)
+      this.resultString = "Product :"
+    },
+    div(){
+      this.resultString = ''
+      if(this.num2 != 0){
+        this.result = (this.num1/this.num2)
+        this.resultString = "Quotient :"
+      }
+      else
+        this.result = 'Divisor cannot be 0'
+    },
+  
+}
+}
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+p{
+  font-weight: bold;
+  font-size: larger;
+  font-style: normal;
+  
+}
+button{
+  margin-left: 7px;
+}
+div{
+  margin-top: 5%;
+}
+.positive{
+  color: red; 
+}
+.negative{
+  color: orange;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+ <template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <input type="text" v-model="name">
+    Hello {{name}}
+   <input type="number" v-model="num">
+    {{num}}
+    
+   <input type="number" v-model="num">
+     {{num}}
+    
+    
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+      name: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
