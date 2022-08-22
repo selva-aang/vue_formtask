4fb6ed2a97fa129ac18b00b59aaf9a9b28dd10ee
selva-aang <selvakumar@ngpwebsmart.com>
2022-08-22 19:49:16 +0530

    taskForm completed

---
+++ src/components/FormOps.vue
@@ -0,0 +1,219 @@
+ 
+            <template>
+                
+                <div class="form-control">
+                    <!-- <input type="text" class="form-control" name="field_1" id="field_1" placeholder="Name" data-msg-required="This field is required" style="background: rgb(255, 255, 255) none repeat scroll 0% 0%;">
+                
+                <input type="email" class="form-control" name="field_2" id="field_2" placeholder="Email" data-msg-required="This field is required" style="background: rgb(255, 255, 255) none repeat scroll 0% 0%;"> -->
+                <!-- <input type="password" class="form-control" name="pass" id="pass"  placeholder="Password" value=""  data-msg-required="This field is required" aria-label="Password"> -->
+                    <!-- <span>Gender: {{ gender }}</span> -->
+                    <!-- <input type="radio" id="one" value="Male" v-model="Gender">
+                    <label for="one">Male</label>
+                    <br>
+                    <input type="radio" id="two" value="Two" v-model="Gender">
+                    <label for="two">Female</label>
+                    <br> -->
+                        
+                <v-text-field
+                v-model="name"
+                :counter="15"
+                :rules="nameRules"
+                label="Name"
+                required
+            ></v-text-field>
+        
+            <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                label="E-mail"
+                required
+            ></v-text-field>
+            <v-text-field
+            v-model="lengthPassword"
+            label="Length password"
+            >
+            </v-text-field>
+            
+            
+        <v-container fluid>
+        <v-row>
+           
+
+            <v-col
+            cols="12"
+            sm="6"
+            >
+            <v-text-field
+                v-model="password"
+                label="password"
+               
+            ></v-text-field>
+            </v-col>
+            <v-col>
+                <v-btn 
+                @click="generatePassword">
+                    Generate
+                </v-btn>
+            </v-col>
+
+            
+
+         
+        </v-row>
+        </v-container>
+    
+            
+            
+    <v-row align="center">
+        <v-col cols="12">
+            <v-combobox
+          v-model="select_the_state"
+          :items="items"
+          label="Select the state"
+          outlined
+          dense
+        ></v-combobox>
+        <!-- <v-select
+            :items="items"
+            select_the_state
+            label="Select_the_state"
+        ></v-select> -->
+        </v-col>
+    </v-row>
+
+    
+            
+            
+                
+
+            
+            
+            
+        
+        
+                <v-radio-group v-model="radioGroup">
+                <v-radio
+                    
+                
+                    label="Male"
+                    value="radio-Male"
+                ></v-radio>
+                <v-radio
+                    label="Female"
+                    value="radio-Female"
+                ></v-radio>
+                
+                
+                </v-radio-group>
+            
+
+                    <!-- <span>Checked boxes: {{ checkBoxes }}</span>
+                    <br>
+                    <input type="checkbox" id="1" value="yes i'm interested in" v-modal="checkBoxes">
+                    <label for="1">yes i'm interested in</label>
+                    <input type="checkbox" id="2" value="yes i'm not interested" v-modal="checkBoxes">
+                    <label for="2">yes i'm not interested </label>
+
+                    <br> -->
+              
+              <v-checkbox
+              v-model="checkBoxes"
+              label="yes i'm interested in"
+              color="blue"
+              value="yes i'm interested in"
+              hide-details
+            ></v-checkbox>
+                 <v-checkbox
+              v-model="checkBoxes"
+              label="yes i'm not interested"
+              color="red"
+              value="yes i'm not interested"
+              hide-details
+            ></v-checkbox>
+            
+                <v-btn
+                    rounded
+                    color="primary"
+                    :disabled="isButtonClick"
+                >
+                 Save 
+                </v-btn>
+                    
+                </div>
+            </template>
+            <script>
+
+
+            export default {
+            data: () => ({
+                
+            lengthPassword: '',
+            valid: true,
+            name: '',
+            password:'',
+            select_the_state: '',
+            radioGroup: [],
+            checkBoxes: [],
+            nameRules: [
+                v => !!v || 'Name is required',
+                v => (v && v.length <= 15) || 'Name must be less than 15 characters',
+            ],
+            email: '',
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            ],
+            items: ['Andhra', 'karantaka', 'tamilnadu','Delhi', 'kolkata'],
+            
+            
+            rules: {
+                required: value => !!value || 'Required.',
+                min: v => v.length >= 8 || 'Min 8 characters',
+                emailMatch: () => (`The email and password you entered don't match`),
+            },
+            
+        
+  
+            
+            }),
+    methods:{
+        generatePassword(){
+    
+            const Allowed = {
+                Uppers: "QWERTYUIOPASDFGHJKLZXCVBNM",
+                Lowers: "qwertyuiopasdfghjklzxcvbnm",
+                Numbers: "1234567890",
+                Symbols: "!@#$%^&*"
+            }
+            const getRandomCharFromString = (str) => str.charAt(Math.floor(Math.random() * str.length))
+            const generate = (length = this.lengthPassword) => { // password will be @Param-length, default to 8, and have at least one upper, one lower, one number and one symbol
+                let pwd = "";
+                pwd += getRandomCharFromString(Allowed.Uppers); //pwd will have at least one upper
+                pwd += getRandomCharFromString(Allowed.Lowers); //pwd will have at least one lower
+                pwd += getRandomCharFromString(Allowed.Numbers); //pwd will have at least one number
+                pwd += getRandomCharFromString(Allowed.Symbols);//pwd will have at least one symbol
+                for (let i = pwd.length; i < length; i++)
+                    pwd += getRandomCharFromString(Object.values(Allowed).join('')); //fill the rest of the pwd with random characters
+                return pwd
+            }
+            this.password = generate(this.lengthPassword)
+        }
+    },
+    computed:{
+        isButtonClick() {
+            console.log('button click')
+        return (
+            this.name.length == 0 ||
+            this.email.length == 0 ||
+            this.lengthPassword.length == 0 ||
+            this.select_the_state.length == 0 ||
+            this.radioGroup.length == 0 ||
+            this.checkBoxes.length == 0
+          
+            
+            )
+          }
+        },
+
+        }
+            </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <hello-world />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/ArithmeticOps.vue'
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  import HelloWorld from '../components/FormOps'
+
+  export default {
+    name: 'HomeView',
+
+    components: {
+      HelloWorld,
+    },
   }
-}
 </script>
